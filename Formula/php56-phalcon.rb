f2ff0c74b5b3f93dc1eb6ac9bb1fea567afd3429
Indra <indra.halimm@gmail.com>
2018-02-20 13:48:18 +0700

    downgrade phalcon version as it contains bug Unknown filter type

--- Formula/php56-phalcon.rb
+++ Formula/php56-phalcon.rb
@@ -4,7 +4,7 @@ class Php56Phalcon < AbstractPhp56Extension
   init
   desc "Full-stack PHP framework"
   homepage "https://phalconphp.com/"
-  url "https://github.com/phalcon/cphalcon/archive/v3.3.0.tar.gz"
+  url "https://github.com/phalcon/cphalcon/archive/v3.2.0.tar.gz"
   sha256 "559211b861a71ae6032216b2dc41d085560354072c95d1000b13fd37b0e0e008"
   head "https://github.com/phalcon/cphalcon.git"
 
