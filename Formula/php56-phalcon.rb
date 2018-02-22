c3e03546dbdf9dc53f413e201e5b6f1c45d065b7
Indra <indra.halimm@gmail.com>
2018-02-22 14:40:35 +0700

    downgrade phalcon to 2.0.13

--- Formula/php56-phalcon.rb
+++ Formula/php56-phalcon.rb
@@ -4,13 +4,13 @@ class Php56Phalcon < AbstractPhp56Extension
   init
   desc "Full-stack PHP framework"
   homepage "https://phalconphp.com/"
-  url "https://github.com/phalcon/cphalcon/archive/v3.2.0.tar.gz"
-  sha256 "32f83fc7ab4eeee37a6577e5920ed5d94f455c723cf9499d195fad3be30c104e"
+  url "https://codeload.github.com/phalcon/cphalcon/tar.gz/phalcon-v2.0.13"
+  sha256 "0a1bd6afe902c6f2f68cf5e2f2785503f5ad95d1d2cf1b66c77154c483a08a35"
   head "https://github.com/phalcon/cphalcon.git"
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "32f83fc7ab4eeee37a6577e5920ed5d94f455c723cf9499d195fad3be30c104e" => :high_sierra
+    sha256 "0a1bd6afe902c6f2f68cf5e2f2785503f5ad95d1d2cf1b66c77154c483a08a35" => :high_sierra
     sha256 "71576380cb9c7ad40577dbda4b38329019f54c0e63d3b3db2c0089e066667bb7" => :sierra
     sha256 "6aa1be9da4bbce712f12fcfddb68b9f7e3eba2f1349ed4d761427cc9d189b3c2" => :el_capitan
   end
